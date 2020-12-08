4c79d1e34ce3c96c48389d56dae0b49c804ff98a
ankouichi <danielsmart.an@gmail.com>
2020-12-08 12:14:58 -0600

    create project

---
+++ PocketVision/PocketVision/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PocketVision
+//
+//  Created by 安浩一 on 12/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
